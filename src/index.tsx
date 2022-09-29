c458913d4063a43d66ab049ce7be6883b70bf27d
iHarryD <anuraagvr555@gmail.com>
2022-09-29 13:19:36 +0530

    style: add global stylesheet and css variables

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./styles/global.css";
+import "./styles/variables.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
